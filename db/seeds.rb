59ed68c52a17c73a5fd9bcc125ba751c8c3b7b9c
Chris Gallagher <cgally103@gmail.com>
2017-12-20 20:46:03 -0500

    complete assignment 21

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,17 @@ end
 
 posts = Post.all
 
+50.times do
+  SponsoredPost.create!(
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph,
+    price:  RandomData.random_integer
+  )
+end
+
+sponsored_posts = SponsoredPost.all
+
 100.times do
   Comment.create!(
     post: posts.sample,
@@ -28,4 +39,5 @@ end
 puts 'Seed finished'
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{SponsoredPost.count} sponsoredposts created"
 puts "#{Comment.count} comments created"
