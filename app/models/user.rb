10e7fbbc51fcf4aec457a019812dd320affd8867
Chris Gallagher <cgally103@gmail.com>
2018-01-02 13:46:33 -0500

    complete checkpoint 27

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  
+
   before_save { self.email = email.downcase if email.present? }
 
   before_save { self.name = name.split.map{ |word| word.capitalize }.join(' ') if name.present? }
+
+  before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
@@ -16,4 +18,6 @@ class User < ApplicationRecord
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  enum role: [:member, :admin]
 end
