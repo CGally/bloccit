c1e78e6c2a1629e594809981aaf8be6274fbafbc
Chris Gallagher <cgally103@gmail.com>
2018-01-03 16:41:23 -0500

    complete assignment 31

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  def total_posts(user)
+    if @user.posts.count == 0
+      return "#{user.name} has not submitted any posts yet."
+    else
+      render @user.posts
+    end
+  end
+
+  def total_comments(user)
+    if @user.comments.count == 0
+      return "#{user.name} has not submitted any comments yet."
+    else
+      render @user.comments
+    end
+  end
 end
